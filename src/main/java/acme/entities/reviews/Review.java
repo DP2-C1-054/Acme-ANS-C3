fa33387fca20a32560cad6298cc314a71101cab4
adrdiavaz <adrdiavaz@alum.us.es>
2025-03-08 19:00:06 +0100

    fix: Added getter, setter and entity annotations
    refs #72

--- src/main/java/acme/entities/reviews/Review.java
+++ src/main/java/acme/entities/reviews/Review.java
@@ -3,6 +3,7 @@ package acme.entities.reviews;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -14,7 +15,12 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class Review extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
