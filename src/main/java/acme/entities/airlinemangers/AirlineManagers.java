07f294878974ab665eb80f0e4a0cd29c16d5f5b9
kevamacal <kevamacal@alum.us.es>
2025-03-09 19:47:42 +0100

    feat: Creada la entidad AirlineManagers

---
+++ src/main/java/acme/entities/airlinemangers/AirlineManagers.java
@@ -0,0 +1,41 @@
+
+package acme.entities.airlinemangers;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Min;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+
+public class AirlineManagers extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String				identifierNumber;
+
+	@Mandatory
+	@Min(0)
+	@Automapped
+	private Integer				experience;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				birthdate;
+
+	@Optional
+	@Automapped
+	private String				linkPicture;
+
+}
