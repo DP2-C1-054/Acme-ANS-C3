b2cdfc2ec17a5386f852b2fb599a8ac4248232a3
kevamacal <kevamacal@alum.us.es>
2025-03-09 19:59:11 +0100

    fix: falta de notacion entity, getter y setter

--- src/main/java/acme/entities/airlinemangers/AirlineManagers.java
+++ src/main/java/acme/entities/airlinemangers/AirlineManagers.java
@@ -4,6 +4,7 @@ package acme.entities.airlinemangers;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Min;
@@ -14,7 +15,12 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class AirlineManagers extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
