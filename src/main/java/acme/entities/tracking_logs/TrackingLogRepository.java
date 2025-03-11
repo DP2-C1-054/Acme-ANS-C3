514a00cc7fdf28c5374a986a327f89dd02876c25
estlopper <estlopper@alum.us.es>
2025-03-11 12:39:31 +0100

    correcciones tras revision refs #118

--- src/main/java/acme/entities/tracking_logs/TrackingLogRepository.java
+++ src/main/java/acme/entities/tracking_logs/TrackingLogRepository.java
@@ -2,6 +2,7 @@
 package acme.entities.tracking_logs;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
@@ -9,7 +10,7 @@ import acme.client.repositories.AbstractRepository;
 @Repository
 public interface TrackingLogRepository extends AbstractRepository {
 
-	@Query("SELECT tl FROM TrackingLog ORDER BY tl.percentage DESC LIMIT 1")
-	Double mayorPorcentaje();
+	@Query("SELECT max(t.percentage) FROM TrackingLog t WHERE t.claim.id = :claimId")
+	Double mayorPorcentaje(@Param("claimId") Long claimId);
 
 }
