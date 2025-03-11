951b9c6d8eb47a0a8a03aad3dd27c1e22aebb3df
kevamacal <kevamacal@alum.us.es>
2025-03-11 12:14:59 +0100

    fix: correcion notacion de validacion

--- src/main/java/acme/entities/legs/Legs.java
+++ src/main/java/acme/entities/legs/Legs.java
@@ -9,10 +9,13 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
 import acme.constraints.ValidFlightNumber;
 import lombok.Getter;
 import lombok.Setter;
@@ -30,31 +33,39 @@ public class Legs extends AbstractEntity {
 	private String				flightNumber;
 
 	@Mandatory
+	@ValidMoment
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date				scheduledDeparture;
 
 	@Mandatory
+	@ValidMoment
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date				scheduledArrival;
 
 	@Mandatory
+	@ValidString
 	@Automapped
 	private String				departureAirport;
 
 	@Mandatory
+	@ValidString
 	@Automapped
 	private String				arrivalAirport;
 
 	@Mandatory
+	@Valid
 	@Automapped
 	private Double				duration;
 
 	@Mandatory
+	@Valid
 	@Enumerated(EnumType.STRING)
 	private Status				status;
 
 	// Esto iria con el tipo aircraft, pero como todavia no esta implementado toca esperar y dejarlo asi provisionalmente
 	@Mandatory
+	@Valid
+	@Automapped
 	private String				aircraft;
 
 
