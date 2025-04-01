4b8fcdf9c388b7e7d9174155f3778ec972b582f0
adrdiavaz <adrdiavaz@alum.us.es>
2025-04-01 15:56:26 +0200

    feat: Commit de avance para poder revisar tareas

--- src/main/java/acme/features/maintenance/TechnicianMaintenanceRecordPublishService.java
+++ src/main/java/acme/features/maintenance/TechnicianMaintenanceRecordPublishService.java
@@ -1,6 +1,45 @@
+
 package acme.features.maintenance;
 
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.maintenance.MaintenanceRecord;
+import acme.realms.technicians.Technician;
 
-public class TechnicianMaintenanceRecordPublishService {
+@GuiService
+public class TechnicianMaintenanceRecordPublishService extends AbstractGuiService<Technician, MaintenanceRecord> {
+
+	private TechnicianMaintenanceRecordRepository repository;
+
+
+	@Override
+	public void authorise() {
+
+	}
+
+	@Override
+	public void load() {
+
+	}
+
+	@Override
+	public void bind(final MaintenanceRecord record) {
+
+	}
+
+	@Override
+	public void validate(final MaintenanceRecord record) {
+		;
+	}
+
+	@Override
+	public void perform(final MaintenanceRecord record) {
+
+	}
+
+	@Override
+	public void unbind(final MaintenanceRecord record) {
+
+	}
 
 }
--- src/main/java/acme/features/maintenance/TechnicianMaintenanceRecordUpdateService.java
+++ src/main/java/acme/features/maintenance/TechnicianMaintenanceRecordUpdateService.java
@@ -3,6 +3,7 @@ package acme.features.maintenance;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.client.components.models.Dataset;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.aircrafts.Aircraft;
@@ -24,16 +25,25 @@ public class TechnicianMaintenanceRecordUpdateService extends AbstractGuiService
 
 		recordId = super.getRequest().getData("id", int.class);
 		aircraft = this.repository.findAircraftById(recordId);
+		status = aircraft != null;
+
+		super.getResponse().setAuthorised(status);
 	}
 
 	@Override
 	public void load() {
+		MaintenanceRecord record;
+		int id;
 
+		id = super.getRequest().getData("id", int.class);
+		record = this.repository.findMaintenanceRecordById(id);
+
+		super.getBuffer().addData(record);
 	}
 
 	@Override
 	public void bind(final MaintenanceRecord record) {
-
+		super.bindObject(record, "moment", "status", "estimatedCost", "notes");
 	}
 
 	@Override
@@ -41,7 +51,18 @@ public class TechnicianMaintenanceRecordUpdateService extends AbstractGuiService
 		;
 	}
 
-	public void perform() {
-
+	@Override
+	public void perform(final MaintenanceRecord record) {
+		this.repository.save(record);
 	}
+
+	@Override
+	public void unbind(final MaintenanceRecord record) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(record, "moment", "status", "estimatedCost", "notes");
+
+		super.getResponse().addData(dataset);
+	}
+
 }
