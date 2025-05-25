751112c6cd49374994cda76e317b984806a419f3
adrdiavaz <adrdiavaz@alum.us.es>
2025-05-25 17:28:09 +0200

    Indices de la parte grupal

--- src/main/java/acme/entities/aircrafts/Aircraft.java
+++ src/main/java/acme/entities/aircrafts/Aircraft.java
@@ -3,7 +3,9 @@ package acme.entities.aircrafts;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -21,6 +23,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @ValidAircraft
+@Table(indexes = {
+	@Index(columnList = "registration_number")
+})
 public class Aircraft extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
