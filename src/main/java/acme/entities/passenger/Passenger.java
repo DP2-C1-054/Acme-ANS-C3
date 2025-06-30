03b918b87494f0381f13596da619f9ec23e1f2e3
Jesús Pons Moris <jesponmor@alum.us.es>
2025-06-30 16:50:09 +0200

    feat: Agregados indices y test con indices

--- src/main/java/acme/entities/passenger/Passenger.java
+++ src/main/java/acme/entities/passenger/Passenger.java
@@ -4,7 +4,9 @@ package acme.entities.passenger;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -26,6 +28,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @ValidPassenger
+@Table(indexes = {
+	@Index(columnList = "passport,customer_id")
+})
 public class Passenger extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
