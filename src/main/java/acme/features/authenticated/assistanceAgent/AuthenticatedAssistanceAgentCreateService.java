ed4c5a295c18a5b5ebe87e2f87fbf774b701e7d3
estlopper <estlopper@alum.us.es>
2025-07-01 23:46:06 +0200

    supplementaria 17 y 18

---
+++ src/main/java/acme/features/authenticated/assistanceAgent/AuthenticatedAssistanceAgentCreateService.java
@@ -0,0 +1,100 @@
+
+package acme.features.authenticated.assistanceAgent;
+
+import java.util.Collection;
+import java.util.Date;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Authenticated;
+import acme.client.components.principals.UserAccount;
+import acme.client.components.views.SelectChoices;
+import acme.client.helpers.MomentHelper;
+import acme.client.helpers.PrincipalHelper;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.airlines.Airline;
+import acme.realms.assistance_agents.AssistanceAgent;
+
+@GuiService
+public class AuthenticatedAssistanceAgentCreateService extends AbstractGuiService<Authenticated, AssistanceAgent> {
+
+	private AuthenticatedAssistanceAgentRepository repository;
+
+
+	@Override
+	public void authorise() {
+		boolean status;
+		String method;
+		Airline airline;
+		int airlineId;
+
+		method = super.getRequest().getMethod();
+
+		if (method.equals("GET"))
+			status = !super.getRequest().getPrincipal().hasRealmOfType(AssistanceAgent.class);
+		else {
+			airlineId = super.getRequest().getData("airline", int.class);
+			airline = this.repository.findAirlineById(airlineId);
+			status = (airlineId == 0 || airline != null) && !super.getRequest().getPrincipal().hasRealmOfType(AssistanceAgent.class);
+		}
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		AssistanceAgent assistanceAgent;
+		Date moment;
+		int userAccountId;
+		UserAccount userAccount;
+
+		moment = MomentHelper.getCurrentMoment();
+		userAccountId = super.getRequest().getPrincipal().getAccountId();
+		userAccount = this.repository.findUserAccountById(userAccountId);
+
+		assistanceAgent = new AssistanceAgent();
+		assistanceAgent.setUserAccount(userAccount);
+		assistanceAgent.setMoment(moment);
+
+		super.getBuffer().addData(assistanceAgent);
+	}
+
+	@Override
+	public void bind(final AssistanceAgent assistanceAgent) {
+		super.bindObject(assistanceAgent, "employeeCode", "spokenLanguages", "moment", "bio", "salary", "photoUrl", "airline");
+	}
+
+	@Override
+	public void validate(final AssistanceAgent assistanceAgent) {
+		String cod = assistanceAgent.getEmployeeCode();
+		AssistanceAgent agent = this.repository.findAssistanceAgentByEmployeeCode(cod);
+		super.state(agent == null, "employeeCode", "acme.validation.assistance-agent.code.message");
+	}
+
+	@Override
+	public void perform(final AssistanceAgent assistanceAgent) {
+		this.repository.save(assistanceAgent);
+	}
+
+	@Override
+	public void unbind(final AssistanceAgent assistanceAgent) {
+		Collection<Airline> airlines;
+		SelectChoices choices;
+		Dataset dataset;
+
+		airlines = this.repository.findAllAirlines();
+		choices = SelectChoices.from(airlines, "name", assistanceAgent.getAirline());
+
+		dataset = super.unbindObject(assistanceAgent, "employeeCode", "spokenLanguages", "moment", "bio", "salary", "photoUrl", "airline");
+		dataset.put("airlines", choices);
+
+		super.getResponse().addData(dataset);
+	}
+
+	@Override
+	public void onSuccess() {
+		if (super.getRequest().getMethod().equals("POST"))
+			PrincipalHelper.handleUpdate();
+	}
+
+}
